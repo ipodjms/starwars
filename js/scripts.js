76c233fa5f521c9ec87552c7f5e3c71d4684600b
João Marcos <jhoudoris@gmail.com>
2018-03-15 03:50:45 -0300

    melhorias finais

--- js/scripts.js
+++ js/scripts.js
@@ -125,6 +125,11 @@ function EncontraInfoPersonagem() {
 
 
 function ComparaNomePersonagem(nome,posicao) {
+
+    if (nome == "") {
+        alert ("Informe o nome do personagem");
+        return false;
+    }
     
     var perguntasCorretas = 0;
 
@@ -165,14 +170,18 @@ function ComparaNomePersonagem(nome,posicao) {
 
         $("#ButtonPersonagem" + posicao).attr("disabled","disabled");
         $("#Personagem" + posicao).removeClass("border border-danger");   
-        $("#Personagem" + posicao).addClass("border border-success");         
+        $("#Personagem" + posicao).removeClass("shake animated");   
+        $("#Personagem" + posicao).addClass("border border-success"); 
+        $("#Personagem" + posicao).addClass("flash animated");        
 
     }
 
     else {
         console.log("Voce errou o nome");
+        $("#Personagem" + posicao).removeClass("shake animated");
         $("#Personagem" + posicao).removeClass("border border-success");
         $("#Personagem" + posicao).addClass("border border-danger");
+        $("#Personagem" + posicao).addClass("shake animated");
     }
 
 
@@ -234,6 +243,32 @@ $( document ).ready(function() {
 
     console.log( "Pronto para detonar!" );
 
+    $.fn.extend({
+      animateCss: function(animationName, callback) {
+        var animationEnd = (function(el) {
+          var animations = {
+            animation: 'animationend',
+            OAnimation: 'oAnimationEnd',
+            MozAnimation: 'mozAnimationEnd',
+            WebkitAnimation: 'webkitAnimationEnd',
+          };
+
+          for (var t in animations) {
+            if (el.style[t] !== undefined) {
+              return animations[t];
+            }
+          }
+        })(document.createElement('div'));
+
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+          $(this).removeClass('animated ' + animationName);
+
+          if (typeof callback === 'function') callback();
+        });
+
+        return this;
+      },
+    });
 
 
 
